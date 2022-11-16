c819595c90ed71398490fbcb2b81b321148907be
nerminbenmohamed <nermin.benmohamed@esprit.tn>
2022-11-16 11:35:07 +0100

    services and controllers

--- src/main/java/tn/spring/springboot/Controller/EtudiantRestController.java
+++ src/main/java/tn/spring/springboot/Controller/EtudiantRestController.java
@@ -1,28 +1,37 @@
 package tn.spring.springboot.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tn.spring.springboot.Service.IEtudiantService;
 import tn.spring.springboot.Entity.Etudiant;
-import tn.spring.springboot.Service.IEtudiantservice;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/etudiant")
-
-
 public class EtudiantRestController {
     @Autowired
-    IEtudiantservice etudiantservice;
-
-
-    @GetMapping("/affichertout")
-
-    List<Etudiant> affichertout(){
-        List<Etudiant> l= Etudiantservice.retrievedALLEtudiants();
+    IEtudiantService etudiantService;
+    //http://localhost:8020/esprit/etudiant/retrieveAll
+    @GetMapping("/retrieveAll")
+    List<Etudiant> retrieveAllEtudiants() {
+        List<Etudiant> l = etudiantService.retrieveAllEtudiants();
         return l;
+    }
+    //http://localhost:8020/esprit/etudiant/addetudiant
 
-}
+    @PostMapping("/addetudiant")
+    Etudiant addEtudiant (@RequestBody Etudiant e){
+        return   etudiantService.addEtudiant(e);
+    }
+    //http://localhost:8020/esprit/etudiant/updateEtudiant
+    @PutMapping("/updateEtudiant")
+    Etudiant updateEtudiant(@RequestBody Etudiant e){
+        return etudiantService.updateEtudiant(e);
+
+    }
+    //http://localhost:8020/esprit/etudiant/deleteEtudiant/{id_etudiant}
+    @DeleteMapping("/deleteEtudiant/{id_etudiant}")
+    void deleteEtudiant(@PathVariable("id_etudiant") long id){
+        etudiantService.deleteEtudiant( id);
+    }
 }
--- src/main/java/tn/spring/springboot/Service/IEtudiantService.java
+++ src/main/java/tn/spring/springboot/Service/IEtudiantService.java
@@ -1,6 +1,18 @@
 package tn.spring.springboot.Service;
 
+import tn.spring.springboot.Entity.Etudiant;
+
+import java.util.List;
+
 public interface IEtudiantService {
+
+    List<Etudiant> retrieveAllEtudiants();
+    Etudiant addEtudiant(Etudiant e);
+    void deleteEtudiant(Long idEtudiant);
+
+    Etudiant retrieveEtudiant(Long idEtudiant, Etudiant e);
+
+    Etudiant updateEtudiant(Etudiant e);
 
 
 }
